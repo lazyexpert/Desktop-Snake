a6b64f0fd2b543265acd701ce4bd68ffc5ef2a2d
lazyexpert <lazyexpert@i.ua>
2015-10-28 18:19:05 +0200

    trying with rendering apple image

--- js/controllers/gameplayController.js
+++ js/controllers/gameplayController.js
@@ -4,12 +4,12 @@ let gameplayController = {
   canvasSelector : '.gameboard',
   cellSize : 30,
   strokeWidth : 5,
-  strokeColor : "black",
+  strokeColor : "red",
   fillColors : {
-    0 : "yellow",
-    1 : "white",
-    2 : "red",
-    3 : "green"
+    0 : "white", // Field Background
+    1 : "rgb(221, 197, 71)", // Snake Body
+    2 : "rgb(33, 79, 219)", // Snake Head
+    3 : "green" // Apple first version
   },
   renderingInterval : null,
   fps : 100,
@@ -20,6 +20,8 @@ let gameplayController = {
   init: function(){
     this.score = 0;
     this.startRendering();
+    this.img = document.createElement('img');
+    this.img.src = "img/apple.png";
 
     document.addEventListener('defeat', this.onDefeat.bind(this));
   },
@@ -27,14 +29,14 @@ let gameplayController = {
     alert('defeat!');
   },
   renderCanvas : function() {
-
-
+    this.renderTotalFill();
     this.renderCanvasFill(gameboard.board);
     this.renderCanvasStroke(gameboard.board);
+    this.renderApples( apples.displayed );
     this.refreshScore();
 
     // If last redraw was within the last second
-    if(new Date().getTime() - this.lastTimeRedraw < 1000 )
+    if( new Date().getTime() - this.lastTimeRedraw < 1000 )
       // Count fps
       this.currfps++;
     else {
@@ -54,7 +56,7 @@ let gameplayController = {
   refreshFps : function() {
     let fpsVal = getElement('.fps-val');
 
-    if( fpsVal.innerHTML != this.realfps )
+    //if( fpsVal.innerHTML != this.realfps )
       fpsVal.innerHTML = this.realfps;
   },
   renderCanvasStroke : function(board) {
@@ -76,6 +78,26 @@ let gameplayController = {
     context.strokeStyle = this.strokeColor;
     context.stroke();
   },
+  renderTotalFill : function() {
+    let el = getElement(this.canvasSelector);
+    let context = el.getContext('2d');
+
+    context.beginPath();
+    context.rect(0,0,el.width, el.height);
+    context.fillStyle = this.fillColors[0];
+    context.fill();
+  },
+  renderApples : function(apples) {
+    let context = getElement(this.canvasSelector).getContext('2d');
+    let size = this.cellSize;
+
+    apples.forEach(function(apple) {
+      let x = apple.col * size;
+      let y = apple.row * size;
+
+      context.drawImage(this.img, x, y);
+    }.bind(this));
+  },
   renderCanvasFill : function(board) {
     var self = this;
     function renderCellsFill(cells, color) {
@@ -93,30 +115,17 @@ let gameplayController = {
     let context = getElement(self.canvasSelector).getContext('2d');
 
     // Spread into different fills
-    // Free cells
-    let freeCells = gameboard.getFreeCells();//[];
-    // board.forEach(function(row) {
-    //   freeCells = freeCells.concat(row.filter( cell => cell.busy == 0 ));
-    // });
-
-    renderCellsFill( freeCells, self.fillColors[0] );
-
     // Busy cells - actually the snake body
     let busyCells = snake.body;
-    // [];
-    //
-    // board.forEach(function(row) {
-    //   busyCells = busyCells.concat(row.filter( cell => cell.busy == 1 ));
-    // });
-    //
-    // if( busyCells.length )
+
     renderCellsFill( busyCells, self.fillColors[1] );
 
     // Snake head
     renderCellsFill( [snake.head], self.fillColors[2]);
 
     // Apples
-    renderCellsFill( apples.displayed, self.fillColors[3]);
+
+    //renderCellsFill( apples.displayed, self.fillColors[3]);
   },
   startRendering : function() {
     this.currfps = 0;
